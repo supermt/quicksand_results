37374be27fe041572115eaf9c2e2c7c91941856b
supermt_PM <18007092757@163.com>
2022-03-15 23:47:40 +0900

    add the results of device influence

---
+++ disk_type_influence/traverse_all_cpu_counts.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+work_path = os.getcwd()
+os.chdir("../")
+print(os.getcwd())
+sys.path.insert(0, '.')
+
+from db_bench_option import DEFAULT_DB_BENCH
+from db_bench_option import load_config_file
+from db_bench_option import set_parameters_to_env
+from db_bench_runner import DB_launcher
+from db_bench_runner import reset_CPUs
+from parameter_generator import HardwareEnvironment
+from parameter_generator import StorageMaterial
+from db_bench_runner import clean_cgroup
+
+os.chdir(work_path)
+if __name__ == '__main__':
+    env = HardwareEnvironment()
+    parameter_dict = load_config_file('config.json')
+    set_parameters_to_env(parameter_dict, env)
+
+
+
+    result_dir = "pm_results_device_influence"
+    
+    benchmark_opt =  {
+            "num":20*1000*1000,
+            "report_interval_seconds": 1,
+            "value_size":1000,
+            "key_size":16,
+            "benchmarks":"fillrandom,readrandom,stats",
+            "subcompactions":20,
+            "reads":100000,
+ 
+            "histogram":"true",
+            "report_bg_io_stats":"true",
+            #"DOTA_enabled":"true",
+        }
+    temp_result_dir = result_dir +"/"
+    runner = DB_launcher(
+        env, temp_result_dir, db_bench=DEFAULT_DB_BENCH, extend_options=benchmark_opt)
+    runner.run()
+    reset_CPUs()
+    clean_cgroup()
